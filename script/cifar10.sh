a47e5b8db9ed5502a8f303069d3e5b330b73202b
WeiQin Chuah <33308266+waychin-weiqin@users.noreply.github.com>
2024-03-12 16:54:08 +1100

    Update cifar10.sh

--- script/cifar10.sh
+++ script/cifar10.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# Get today's date in YYYY-MM-DD format
-now=$(date +"%Y-%m-%d_%H-%M-%S")
-
-# Name of the log file
-logfile="training_log_${now}_cifar10.log"
-
 # Set the GPU ID to use
 # Replace '0' with the ID of the GPU you want to use
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 python -u main.py --model_path models/Hendrycks2020AugMixWRN_c10.pt \
                   --data_path /home/wei/data2/Dataset/cifar/ \
