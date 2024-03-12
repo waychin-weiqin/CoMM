1396fa8048da1a0a401d87e4f635db19a89d2abe
WeiQin Chuah <33308266+waychin-weiqin@users.noreply.github.com>
2024-03-12 16:53:29 +1100

    Update cifar100.sh

--- script/cifar100.sh
+++ script/cifar100.sh
@@ -10,26 +10,11 @@ logfile="training_log_${now}_cifar100.log"
 # Replace '0' with the ID of the GPU you want to use
 export CUDA_VISIBLE_DEVICES=6
 
-#python -u main.py --model_path models/Hendrycks2020AugMixWRN_c100.pt \
-#                  --data_path /home/wei/data2/Dataset/cifar/ \
-#                  --source_dataset cifar-100 \
-#                  --target_dataset cifar-100-c \
-#                  --optimize bn \
-#                  --lr 0.0025 \
-#                  --tta_batchsize 128 \
-#                  --severity 5 \
-#                  --criterion entropy \
-#                  --network wrn-40x2 \
-#                  --cosine_weight \
-#                  >> "$logfile" 2>&1
-
-
 python -u main.py --model_path models/Hendrycks2020AugMixWRN_c100.pt \
                   --data_path /home/wei/data2/Dataset/cifar/ \
                   --source_dataset cifar-100 \
                   --target_dataset cifar-100-c \
-                  --optimize bn \
-                  --lr 0.00025 \
+                  --lr 0.005 \
                   --tta_batchsize 128 \
                   --severity 5 \
                   --criterion entropy \
